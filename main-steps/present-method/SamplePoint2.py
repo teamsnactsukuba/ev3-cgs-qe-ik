026c6e94e2659320044719727a10f71ad1064c2b
Akira Terui <akira.terui@gmail.com>
2022-05-08 00:07:05 +0900

    Examples of inverse kinematics computation added

---
+++ main-steps/present-method/SamplePoint2.py
@@ -0,0 +1,37 @@
+from math import sqrt
+from ctypes import * 
+from sympy import *
+from sympy import im
+from sympy import polys
+from scipy.optimize import fsolve
+from fractions import Fraction
+from decimal import Decimal
+import random
+import time
+import numpy as np
+import sympy as sp
+
+x = Symbol('x')
+y = Symbol('y')
+z = Symbol('z')
+s_1 = Symbol('s_1')
+c_1 = Symbol('c_1')
+s_4 = Symbol('s_4')
+s_7 = Symbol('s_7')
+c_4 = Symbol('c_4')
+c_7 = Symbol('c_7')
+t = Symbol('t')
+
+#len : 1000
+#Points=Points0--Points9の結合
+Points = [[Fraction(-6061, 41), Fraction(-7679, 51), Fraction(4379, 27)],[0, 0, 200],[300,400,500],[300,0,400]]
+
+
+
+
+
+
+
+
+
+
